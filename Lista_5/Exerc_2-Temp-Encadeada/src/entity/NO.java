76246aeb15d6c6e6b6e1a5e4794907d2d5876e7d
CTRL_Marcws <marfilipi@hotmail.com>
2021-04-07 22:44:14 -0300

    Lista_5 - Primeiro Commit

---
+++ Lista_5/Exerc_2-Temp-Encadeada/src/entity/NO.java
@@ -0,0 +1,28 @@
+package entity;
+
+public class NO
+{
+	private double temp;
+	private NO prox;
+	
+	
+	public double getTemp()
+	{
+		return temp;
+	}
+	
+	public void setTemp(double temp)
+	{
+		this.temp = temp;
+	}
+	
+	public NO getProx()
+	{
+		return prox;
+	}
+	
+	public void setProx(NO prox)
+	{
+		this.prox = prox;
+	}
+}
